e592a3e172ad2ef5be797076d8b2ec4deae6e891
Victor Sriqui <victorsriqui@gmail.com>
2023-04-11 17:39:26 -0600

    feat: Add new palette  button

---
+++ main.js
@@ -0,0 +1,22 @@
+// ** Color Boxes ** //
+var box1 = document.querySelector(".boxes__box1");
+var box2 = document.querySelector(".boxes__box2");
+var box3 = document.querySelector(".boxes__box3");
+var box4 = document.querySelector(".boxes__box4");
+var box5 = document.querySelector(".boxes__box5");
+
+// ** Color Hexes ** //
+var hex1 = document.querySelector(".boxes__hex1");
+var hex2 = document.querySelector(".boxes__hex2");
+var hex3 = document.querySelector(".boxes__hex3");
+var hex4 = document.querySelector(".boxes__hex4");
+var hex5 = document.querySelector(".boxes__hex5");
+
+// ** Buttons ** //
+var newButton = document.querySelector(".buttons__new");
+
+// ** Event Listeners ** //
+
+newButton.onclick = function(event) {
+    
+}
